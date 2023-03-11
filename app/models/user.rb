33d5139c9087e65f76b3a66084c9668d75178625
abeyaworld <127001684+TekuToko@users.noreply.github.com>
2023-03-11 19:14:54 +0900

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  validates :nickname, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "6文字以上、かつ半角英数を両方含む必要があります" }
+  validates :birthday, presence: true
+  validates :sex_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :car_size_id, numericality: { other_than: 1 }
+  validates :driver_history_id, numericality: { other_than: 1 }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'warehouses/index'
   root to: "warehouses#index"
 end
