eb133f3d30a34160bfc1ce3b234ffd2ada4d38bc
abeyaworld <127001684+TekuToko@users.noreply.github.com>
2023-03-12 00:12:10 +0900

    ActiveHash修正＋ストロングパラメーター設定

--- app/models/car_size.rb
+++ app/models/car_size.rb
@@ -1,4 +1,4 @@
-class Category < ActiveHash::Base
+class CarSize < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '~2t' },
--- app/models/driver_history.rb
+++ app/models/driver_history.rb
@@ -1,4 +1,4 @@
-class Category < ActiveHash::Base
+class DriverHistory < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '~1年' },
