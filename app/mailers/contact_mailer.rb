5207302f5c9b1af6a77a3a098ef6498233f7d6cf
abeyaworld <127001684+TekuToko@users.noreply.github.com>
2023-03-16 00:37:21 +0900

    お問い合わせフォーム２

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,7 @@ class ContactMailer < ApplicationMailer
 
   def send_mail(contact)
     @contact = contact
-    mail(from: contact.email, to: ENV['MAIL_ADDRESS'], subject: 'Webサイトより問い合わせが届きました') do |format|
+    mail(from: contact.email, to: ENV['TOMAIL'], subject: 'Webサイトより問い合わせが届きました') do |format|
       format.text
     end
   end
